ef745ff39dfbd0ff20002421fcc05dd8826fbac6
Hagen Schupp <hagen.schupp@rwth-aachen.de>
2025-08-31 22:26:54 +0200

    Addressing Servo jitter by ensuring changes are above servo dead-band duty cycle changes

--- backend/board.py
+++ backend/board.py
@@ -7,9 +7,11 @@ class ServoBoard:
     CMD_SERVO2 = 1
     CMD_SERVO3 = 2
     CMD_SERVO4 = 3
+    # https://www.friendlywire.com/projects/ne555-servo-safe/SG90-datasheet.pdf
     SERVO_MAX_ANGLE = 180
     SERVO_MIN_ANGLE = 0
-    SERVO_MAX_PULSE_WIDTH = 2500
+    # Servo PWM duty cycle in mu sec
+    SERVO_MAX_PULSE_WIDTH = 2400
     SERVO_MIN_PULSE_WIDTH = 500
     def __init__(self, addr=0x18):
         self.address = addr
@@ -37,6 +39,9 @@ class ServoBoard:
     def set_angle(self, target, angle):
         if (angle < self.SERVO_MIN_ANGLE) or (angle > self.SERVO_MAX_ANGLE):
             raise Exception('Invalid angle')
+        
+        # Dead band-width: 7us -> ~1° resolution
+        angle = round(angle)
 
         pwm = self._calculate_pwm(angle)
         self._write_registers(target, pwm)      
